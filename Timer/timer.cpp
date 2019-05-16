3bf68f3dfdc9dc3e7ac42908bb95e4a3064a0a11
taeyeong201 <dkwkrh201@naver.com>
2019-05-16 23:19:42 +0900

    Timer 추가

---
+++ Timer/timer.cpp
@@ -0,0 +1,21 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+typedef long long int64;
+
+LARGE_INTEGER Frequency;
+LARGE_INTEGER BeginTime;
+LARGE_INTEGER Endtime;
+int64 elapsed;
+
+int main() {
+	QueryPerformanceFrequency(&Frequency);
+	for (int i = 0; i < 5; i++) {
+		QueryPerformanceCounter(&BeginTime);
+		Sleep(2000);
+		QueryPerformanceCounter(&Endtime);
+		elapsed = Endtime.QuadPart - BeginTime.QuadPart;
+		float duringtime = elapsed / Frequency.QuadPart;
+		printf("%f \n", duringtime);
+	}
+}
